4c7954e710d93365715a654cc6a888f581eaa690
Thao Nguyen <thaontt1408@gmail.com>
2022-09-02 17:01:26 -0600

    complete find stocks by sectors

--- StockTracker/src/main/java/com/skillsdistillery/stocktracker/controllers/StockController.java
+++ StockTracker/src/main/java/com/skillsdistillery/stocktracker/controllers/StockController.java
@@ -2,8 +2,11 @@ package com.skillsdistillery.stocktracker.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,15 @@ public class StockController {
 	@GetMapping("stocks")
 	public List<Stock> index(){
 		return stockServ.stockList();
+	}
+	
+	@GetMapping("sectors/{sectorId}/stocks")
+	public List<Stock> findStocksBySector(@PathVariable int sectorId, HttpServletResponse res){
+		List<Stock> stocks = stockServ.findStocksBySector(sectorId);
+		if (stocks == null) {
+			res.setStatus(404);
+		}
+		return stocks;
 	}
 
 }
