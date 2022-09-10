808157a9e67a4844809e13f856e44274b581b859
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-09 20:30:31 -0600

    complete show stocks by sector and add form for adding stock

--- StockTracker/src/main/resources/static/js/script.js
+++ StockTracker/src/main/resources/static/js/script.js
@@ -1,28 +1,30 @@
 window.addEventListener('load', function() {
 	console.log('script.js load');
 	init();
+
+	
 });
 
 function init() {
 	//TODO:
-	
+
 	//-Event handlers for buttons and stuff
 	loadAllEvents();
 }
 
 function loadAllEvents() {
-	getStocks();
+	//getStocks();
 	getSectors();
 }
 function getStocks() {
-		let xhr = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "api/stocks");
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 200) {
-				displayStocks(JSON.parse(xhr.responseText));
+				//displayStocks(JSON.parse(xhr.responseText));
 			}
-			 else {
+			else {
 				console.error("Error loading events: " + xhr.status);
 			}
 		}
@@ -30,17 +32,7 @@ function getStocks() {
 	xhr.send();
 }
 
-function displayStocks(stocks) {
-	let dataDiv = document.getElementById("stockList");
-	dataDiv.textContent = '';
-	let ul = document.createElement('ul');
-	dataDiv.appendChild(ul);
-	for (let stock of stocks) {
-		let li = document.createElement('li');
-		li.textContent = stock.symbol;
-		ul.appendChild(li);
-	}
-}
+
 function getSectors() {
 	let xhr = new XMLHttpRequest();
 	xhr.open("GET", "api/sectors");
@@ -50,7 +42,7 @@ function getSectors() {
 				displaySectors(JSON.parse(xhr.responseText));
 				console.log(JSON.parse(xhr.responseText));
 			}
-			 else {
+			else {
 				console.error("Error loading events: " + xhr.status);
 			}
 		}
@@ -68,5 +60,48 @@ function displaySectors(sectors) {
 		let li = document.createElement('li');
 		li.textContent = sector.name;
 		ul.appendChild(li);
+		li.addEventListener('click', function(event) {
+			let sectorHeader = document.getElementById("sectorHeader");			
+			sectorHeader.textContent = "Stocks under sector: " +  li.textContent;	
+			getStocksBySector(sector.id);
+		})
 	}
 }
+
+function getStocksBySector(sectorId) {
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", "api/sectors/" + sectorId + "/stocks");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				displayStocksBySector(JSON.parse(xhr.responseText));//convert json to js object
+				console.log(JSON.parse(xhr.responseText));
+			}
+			else {
+				console.error("Error loading events: " + xhr.status);
+			}
+		}
+	};
+	xhr.send();
+}
+
+function displayStocksBySector(stocks) {
+	let stockDiv = document.getElementById("stockList");
+	stockDiv.textContent = '';
+	let ul = document.createElement('ul');
+	stockDiv.appendChild(ul);
+
+	if (stocks.length == 0) {
+		let p = document.createElement('p');
+		p.textContent = "There is no stock found.";
+		stockDiv.appendChild(p);
+	} else {
+		for (let stock of stocks) {
+			let li = document.createElement('li');
+			li.textContent = stock.symbol;
+			ul.appendChild(li);
+		}
+	}
+
+}
+
